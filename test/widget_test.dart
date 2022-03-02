e46fd817570ee01b2b222678f39a5bc51de86ba4
ChengQing <darkblue035@gmail.com>
2022-03-02 14:38:35 +0800

    rename app to jc_dictionary

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wudao_dictionary/main.dart';
+import 'package:jc_dictionary/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
