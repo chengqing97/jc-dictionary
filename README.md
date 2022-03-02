0195d79ddcd3d02e85ca8b892879b9b9cf3c027f
ChengQing <darkblue035@gmail.com>
2022-03-03 02:21:13 +0800

    configure to publish to GooglePlay

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # 简单粗暴实用小词典
+
 Extremely simple and functional mobile dictionary grabbing definitions and translation from [Youdao Dictionary](https://www.youdao.com/) and voice from [Cambridge Dictionary](https://dictionary.cambridge.org/).
 
-
 ### To-dos
+
 - [ ] Disable rotating
 - [ ] Better "previous" function
-- [ ] Welcome message
-- [ ] Voice buttons animation
--- lib/lib.dart
+++ lib/lib.dart
@@ -45,13 +45,15 @@ Future<LookupResult> lookup(String toSearch, http.Client client) async {
     final isTranslate = RegExp(r'<div id="fanyiToggle">').hasMatch(response);
 
     if (isTranslate) {
-      return LookupResult(keyword: toSearch, definition: grabTranslation(response));
+      return LookupResult(
+          keyword: toSearch, definition: grabTranslation(response));
     }
 
     final isChinese = RegExp(r'英语怎么说').hasMatch(response);
 
     if (isChinese) {
-      return LookupResult(keyword: toSearch, definition: grabEnglishWords(response));
+      return LookupResult(
+          keyword: toSearch, definition: grabEnglishWords(response));
     }
 
     final hasResult = RegExp(r'<h2 class="wordbook-js">').hasMatch(response);
@@ -83,23 +85,38 @@ Future<LookupResult> lookup(String toSearch, http.Client client) async {
 }
 
 String? grabTranslation(String response) {
-  final definitionPart = RegExp(r'(?<=<div class="trans-container">)[\s\S]*?(?=</div>)').firstMatch(response)?.group(0);
+  final definitionPart =
+      RegExp(r'(?<=<div class="trans-container">)[\s\S]*?(?=</div>)')
+          .firstMatch(response)
+          ?.group(0);
 
   if (definitionPart == null) return null;
 
-  final definition = RegExp(r'(?<=<p>).*(?=</p>)').allMatches(definitionPart).toList()[1].group(0);
+  final definition = RegExp(r'(?<=<p>).*(?=</p>)')
+      .allMatches(definitionPart)
+      .toList()[1]
+      .group(0);
   return definition;
 }
 
 String? grabEnglishWords(String response) {
-  final definitionPart = RegExp(r'(?<=<div class="trans-container">)[\s\S]*?(?=</div>)').firstMatch(response)?.group(0);
-  final definitionsRaw = RegExp(r'(?<=<p class="wordGroup">)[\s\S]*?(?=</p>)').allMatches(definitionPart ?? "");
+  final definitionPart =
+      RegExp(r'(?<=<div class="trans-container">)[\s\S]*?(?=</div>)')
+          .firstMatch(response)
+          ?.group(0);
+  final definitionsRaw = RegExp(r'(?<=<p class="wordGroup">)[\s\S]*?(?=</p>)')
+      .allMatches(definitionPart ?? "");
   var definitionLines = <String>[];
   for (var item in definitionsRaw) {
-    final wordType = RegExp(r'(?<=;">)[\s\S]*?(?=</span>)').firstMatch(item.group(0) ?? "")?.group(0);
-    final def = RegExp(r'(?<=E2Ctranslation">)[\s\S]*?(?=</a>)').firstMatch(item.group(0) ?? "")?.group(0);
+    final wordType = RegExp(r'(?<=;">)[\s\S]*?(?=</span>)')
+        .firstMatch(item.group(0) ?? "")
+        ?.group(0);
+    final def = RegExp(r'(?<=E2Ctranslation">)[\s\S]*?(?=</a>)')
+        .firstMatch(item.group(0) ?? "")
+        ?.group(0);
     var definitionLine = "";
-    if (wordType != null) definitionLine += "$wordType ";
+    if (wordType != null && !wordType.contains(";"))
+      definitionLine += "$wordType ";
     definitionLine += "$def";
     definitionLines.add(definitionLine);
   }
@@ -110,36 +127,48 @@ String? grabEnglishWords(String response) {
 
 String? grabFormalDefinition(String response) {
   final definitionPart =
-      RegExp(r'(?<=<div class="trans-container">\s*<ul>)[\s\S]*?(?=</ul>)').firstMatch(response)?.group(0);
-  final definitionsRaw = RegExp(r'(?<=<li>)[\s\S]*?(?=</li>)').allMatches(definitionPart ?? "");
+      RegExp(r'(?<=<div class="trans-container">\s*<ul>)[\s\S]*?(?=</ul>)')
+          .firstMatch(response)
+          ?.group(0);
+  final definitionsRaw =
+      RegExp(r'(?<=<li>)[\s\S]*?(?=</li>)').allMatches(definitionPart ?? "");
   if (definitionsRaw.isEmpty) return null;
-  final definition = definitionsRaw.map((item) => item.group(0) ?? "").join("\n");
+  final definition =
+      definitionsRaw.map((item) => item.group(0) ?? "").join("\n");
 
   return definition;
 }
 
 String? grabUkPronunciation(String response) {
-  final ukPronunciation = RegExp(r'(?<=<span class="pronounce">英[\s\S]*<span class="phonetic">)[\s\S]*?(?=</span>)')
+  final ukPronunciation = RegExp(
+          r'(?<=<span class="pronounce">英[\s\S]*<span class="phonetic">)[\s\S]*?(?=</span>)')
       .firstMatch(response)
       ?.group(0);
   return ukPronunciation;
 }
 
 String? grabUsPronunciation(String response) {
-  final usPronunciation = RegExp(r'(?<=<span class="pronounce">美[\s\S]*<span class="phonetic">)[\s\S]*?(?=</span>)')
+  final usPronunciation = RegExp(
+          r'(?<=<span class="pronounce">美[\s\S]*<span class="phonetic">)[\s\S]*?(?=</span>)')
       .firstMatch(response)
       ?.group(0);
   return usPronunciation;
 }
 
 List<Suggestion>? grabSuggestion(String response) {
-  final suggestionsRaw = RegExp(r'(?<=<p class="typo-rel">)[\s\S]*?(?=</p>)').allMatches(response);
+  final suggestionsRaw =
+      RegExp(r'(?<=<p class="typo-rel">)[\s\S]*?(?=</p>)').allMatches(response);
   if (suggestionsRaw.isEmpty) return null;
   var suggestions = <Suggestion>[];
 
   for (var item in suggestionsRaw) {
-    final word = RegExp(r'(?<=class="search-js">)[\s\S]*?(?=</a></span>)').firstMatch(item.group(0) ?? "")?.group(0);
-    final def = RegExp(r'(?<=</span>)[\s\S]*').firstMatch(item.group(0) ?? "")?.group(0)?.trim();
+    final word = RegExp(r'(?<=class="search-js">)[\s\S]*?(?=</a></span>)')
+        .firstMatch(item.group(0) ?? "")
+        ?.group(0);
+    final def = RegExp(r'(?<=</span>)[\s\S]*')
+        .firstMatch(item.group(0) ?? "")
+        ?.group(0)
+        ?.trim();
 
     if (word != null) suggestions.add(Suggestion(word, def));
   }
@@ -155,13 +184,18 @@ class VoiceUrl {
 }
 
 Future<VoiceUrl> getVoiceUrl(String keyword, http.Client client) async {
-  final url = Uri.parse("https://dictionary.cambridge.org/dictionary/english/$keyword");
+  final url =
+      Uri.parse("https://dictionary.cambridge.org/dictionary/english/$keyword");
   var response = await client.read(url, headers: {"user-agent": 'curl/7.77.0'});
 
-  var ukPath = RegExp(r"media/english/uk_pron/[\S]*.mp3").firstMatch(response)?.group(0);
-  var usPath = RegExp(r"media/english/us_pron/[\S]*.mp3").firstMatch(response)?.group(0);
+  var ukPath =
+      RegExp(r"media/english/uk_pron/[\S]*.mp3").firstMatch(response)?.group(0);
+  var usPath =
+      RegExp(r"media/english/us_pron/[\S]*.mp3").firstMatch(response)?.group(0);
 
   const host = "https://dictionary.cambridge.org/";
 
-  return VoiceUrl(uk: ukPath == null ? null : host + ukPath, us: usPath == null ? null : host + usPath);
+  return VoiceUrl(
+      uk: ukPath == null ? null : host + ukPath,
+      us: usPath == null ? null : host + usPath);
 }
